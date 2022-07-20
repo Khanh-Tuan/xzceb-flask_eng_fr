cbf6270761d87c3b09f281c872c46a9807476ed7
Khanh-Tuan <khanh.damtuan@icloud.com>
2022-07-20 09:09:15 +0000

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,16 +4,14 @@ from translator import french_to_english
 
 class TestenglishToFrench(unittest.TestCase):
     def test1(self):
-        self.assertEqual(english_to_french('Null'), 'Null')
+        self.assertNotEqual(english_to_french(0), 0)
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertEqual(english_to_french('Good bye'), 'Au revoir')
-        self.assertNotEqual(english_to_french('Hello'), 'Hello')
+        self.assertEqual(english_to_french('Goodbye'), 'Au revoir')
 
 class TestfrenchToEnglish(unittest.TestCase):
     def test2(self):
-        self.assertEqual(french_to_english('Null'), 'Null')
+        self.assertNotEqual(french_to_english(0), 0)
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertEqual(french_to_english('Au revoir'), 'Good bye')
-        self.assertNotEqual(french_to_english('Bonjour'), 'Bonjour')
+        self.assertEqual(french_to_english('Au revoir'), 'Goodbye')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -10,15 +10,27 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-def english_to_french(french_text):
+authenticator = IAMAuthenticator('KR4kyTwHDV64O8Hy6sX_w2ziGnfmVKZB1Twi04Qt-HW-')
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator= authenticator
+)
+language_translator.set_service_url('https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/59dd7563-8518-4c1b-a39d-ad2247fa92cc')
+
+
+def english_to_french(english_text):
     """
     This function translates English to French
     """
+    translation = language_translator.translate(text=english_text, model_id="en-fr").get_result()
+    french_text=translation['translations'][0]['translation']
     return french_text
 
-def french_to_english(english_text):
+def french_to_english(french_text):
     """
     This function translates French to English
     """
+    translation = language_translator.translate(text=french_text, model_id="fr-en").get_result()
+    english_text=translation['translations'][0]['translation']
     return english_text
 
