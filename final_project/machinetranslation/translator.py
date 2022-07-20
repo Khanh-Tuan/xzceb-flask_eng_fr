33f6c6c8e2c207e794e4f6b2427ff21c4fe9fd10
Khanh-Tuan <khanh.damtuan@icloud.com>
2022-07-20 04:55:27 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,8 +1,10 @@
-"""
-Translator
-"""
 import os
+import json
+
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
+
 load_dotenv()
 
 apikey = os.environ['apikey']
